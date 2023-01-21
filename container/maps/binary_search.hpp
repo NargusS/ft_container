2f1f60f69af0a41167741217a8a00693189f94bc
Alexandre Chane-Law <narguss@airdealexandre.home>
2023-01-21 19:03:10 +0100

    fix search parent

--- container/maps/binary_search.hpp
+++ container/maps/binary_search.hpp
@@ -6,7 +6,7 @@
 /*   By: achane-l <achane-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 09:58:02 by achane-l          #+#    #+#             */
-/*   Updated: 2023/01/21 18:17:16 by achane-l         ###   ########.fr       */
+/*   Updated: 2023/01/21 19:00:33 by achane-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,15 @@ class Node{
 	~Node(){};
 };
 
-Node	*SearchParentValue(Node *parent, int value){
-	while (parent){
-		if ((value < parent->value && !parent->left_child) || (value >= parent->value && !parent->right_child))
-			return (parent);
-		else if (value < parent->value && parent->left_child)
-			parent = parent->left_child;
-		else if (value >= parent->value && parent->right_child)
-			parent = parent->right_child;
+Node	*SearchParentValue(Node *node, int value){
+	Node *parent = NULL;
+
+	while (node){
+		parent = node;
+		if (value < node->value)
+			node = node->left_child;
+		else if (value > node->value)
+			node = node->right_child;
 		else
 			return(parent);// break faire quelque chose
 	}
@@ -52,30 +53,51 @@ Node	*getUncle(Node *parent){
 }
 
 void	left_rotation(Node **root, Node *node){
+	Node *parent;
 	Node *child;
 
 	child = node->right_child;
+	parent = node->parent;
 
 	node->right_child = child->left_child;
+	if (child->left_child)
+		child->left_child->parent = node;
 	child->left_child = node;
-	child->parent = node->parent;
 	node->parent = child;
 
-	if (!child->parent)
+	if (!parent)
 		*root = child;
+	else if (parent->left_child == node)
+		parent->left_child = child;
+	else if (parent->right_child == node)
+		parent->right_child = child;
+
+	if (child)
+		child->parent = parent;
 }
 
 void	right_rotation(Node **root, Node *node){
+	Node *parent;
 	Node *child;
 
 	child = node->left_child;
+	parent = node->parent;
+
 	node->left_child = child->right_child;
+	if (child->right_child)
+		child->right_child->parent = node;
 	child->right_child = node;
-	child->parent = node->parent;
 	node->parent = child;
 
-	if (!child->parent)
+	if (!parent)
 		*root = child;
+	else if (parent->left_child == node)
+		parent->left_child = child;
+	else if (parent->right_child == node)
+		parent->right_child = child;
+
+	if (child)
+		child->parent = parent;
 }
 
 void	fix_properties(Node **root, Node *node){
@@ -121,10 +143,10 @@ void	fix_properties(Node **root, Node *node){
 }
 
 void	addNode(Node **root,int value){
-	Node *parent = *root;
-	Node *new_node = 0;
-	if (!parent){
-		*root = new Node(BLACK, value, 0);
+	Node *parent = NULL;
+	Node *new_node = NULL;
+	if (!(*root)){
+		*root = new Node(BLACK, value,NULL);
 		return;
 	}
 	parent = SearchParentValue(*root, value);
@@ -133,7 +155,7 @@ void	addNode(Node **root,int value){
 		parent->left_child = new_node;
 	else if (value >= parent->value)
 		parent->right_child = new_node;
-	// fix_properties(root, new_node);
+	fix_properties(root, new_node);
 }
 
 int check_rule_4(Node *root)
@@ -171,7 +193,7 @@ void print2DUtil(Node* root, int space)
         return;
  
     // Increase distance between levels
-    space += 5;
+    space += 10;
  
     // Process right child first
     print2DUtil(root->right_child, space);
@@ -179,7 +201,7 @@ void print2DUtil(Node* root, int space)
     // Print current node after space
     // count
     std::cout << std::endl;
-    for (int i = 5; i < space; i++)
+    for (int i = 10; i < space; i++)
         std::cout << " ";
     std::cout << root->value << "["<< root->color <<"]" << "\n";
  
