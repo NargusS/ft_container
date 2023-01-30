9f2289dd568e70d818d226508bd24e6e4265eb39
Alexandre Chane-Law <narguss@airdealexandre.home>
2023-01-31 00:37:43 +0100

    new test

--- test_unit/test.py
+++ test_unit/test.py
@@ -2,7 +2,8 @@ import random
 import n2w
 
 # print(' '.join(str(e) for e in random.sample(range(10), 10)))
-lst = random.sample(range(10), 10);
+lst = random.sample(range(10000), 10000);
 
 for i in lst:
-	print(n2w.convert(i).replace(" ", "") + " " + str(i),end=" ");
+	# print(n2w.convert(i).replace(" ", "") + " " + str(i),end=" ");
+	print("test_insert.insert(lib_used::make_pair(std::string(\"" +n2w.convert(i).replace(" ", "") + "\")," + str(i) + "));");
